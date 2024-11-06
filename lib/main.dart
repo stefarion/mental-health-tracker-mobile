035d0b4fc877632347591a947c39fc8663a3c934
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2024-11-06 15:57:00 +0700

    feat: Follow Tutorial 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/menu.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
+import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart';
+
 
 class MyHomePage extends StatelessWidget {
   final String npm = '2306152456'; // NPM
@@ -36,9 +40,13 @@ class MyHomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        // Mengganti warna icon drawer menjadi putih
+        iconTheme: const IconThemeData(color: Colors.white),
         // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      drawer: const LeftDrawer(),
       // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -164,6 +172,20 @@ class ItemCard extends StatelessWidget {
             ..showSnackBar(
               SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
             );
+            // Navigate ke route yang sesuai (tergantung jenis tombol)
+            if (item.name == "Tambah Mood") {
+              // Navigator.push untuk melakukan navigasi ke MaterialPageRoute yang mencakup MoodEntryFormPage.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MoodEntryFormPage(),
+                ),
+              );
+            } else if (item.name == "Lihat Mood") {
+
+            } else if (item.name == "Logout") {
+
+            }
         },
         // Container untuk menyimpan Icon dan Text
         child: Container(
@@ -191,5 +213,4 @@ class ItemCard extends StatelessWidget {
       ),
     );
   }
-  
 }
